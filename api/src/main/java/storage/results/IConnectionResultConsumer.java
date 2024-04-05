3667c7ceaeb04a97bd6670b644c6df66d9295104
nicollassilva <lyod.hp@gmail.com>
2024-04-05 11:29:43 -0300

    Adding insert, insertBatch, update, updateBatch and delete methods in ConnectionRepository class

--- api/src/main/java/storage/results/IConnectionResultConsumer.java
+++ api/src/main/java/storage/results/IConnectionResultConsumer.java
@@ -2,4 +2,5 @@ package storage.results;
 
 public interface IConnectionResultConsumer {
     void accept(IConnectionResult result) throws Exception;
+    void accept(boolean result) throws Exception;
 }
--- storage/src/main/java/repositories/ConnectionRepository.java
+++ storage/src/main/java/repositories/ConnectionRepository.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import results.ConnectionResult;
 import storage.IConnection;
+import storage.data.IConnectionStatementConsumer;
 import storage.repositories.IConnectionRepository;
 import storage.results.IConnectionResult;
 import storage.results.IConnectionResultConsumer;
@@ -12,6 +13,7 @@ import storage.results.IConnectionResultConsumer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ConnectionRepository implements IConnectionRepository {
     @Inject
@@ -38,9 +40,140 @@ public class ConnectionRepository implements IConnectionRepository {
                 resultConsumer.accept(connectionResult);
             }
         } catch (Exception e) {
-            this.logger.error(STR."Error while executing select query: \{query}", e);
+            this.logger.error(STR."Error while executing SELECT query: \{query}", e);
         } finally {
             this.connection.getConnectionContext().getProvider().closeResultSet(selectResult);
+            this.connection.getConnectionContext().getProvider().closeStatement(preparedStatement);
+            this.connection.getConnectionContext().getProvider().closeConnection(connection);
+        }
+    }
+
+    public void updateBatch(String query, IConnectionStatementConsumer statementConsumer, IConnectionResultConsumer resultConsumer) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet updateResult = null;
+
+        try {
+            connection = this.connection.get();
+            preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            statementConsumer.accept(preparedStatement);
+
+            preparedStatement.executeBatch();
+
+            updateResult = preparedStatement.getGeneratedKeys();
+
+            final IConnectionResult connectionResult = new ConnectionResult(updateResult);
+
+            while (updateResult.next()) {
+                resultConsumer.accept(connectionResult);
+            }
+        } catch (Exception e) {
+            this.logger.error(STR."Error while executing UPDATE query: \{query}", e);
+        } finally {
+            this.connection.getConnectionContext().getProvider().closeResultSet(updateResult);
+            this.connection.getConnectionContext().getProvider().closeStatement(preparedStatement);
+            this.connection.getConnectionContext().getProvider().closeConnection(connection);
+        }
+    }
+
+    public void update(String query, IConnectionResultConsumer consumer, Object... parameters) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        boolean isUpdated;
+
+        try {
+            connection = this.connection.get();
+            preparedStatement = connection.prepareStatement(query);
+
+            this.setPreparedStatementParameters(preparedStatement, parameters);
+
+            isUpdated = preparedStatement.executeUpdate() > 0;
+
+            consumer.accept(isUpdated);
+        } catch (Exception e) {
+            this.logger.error(STR."Error while executing UPDATE query: \{query}", e);
+        } finally {
+            this.connection.getConnectionContext().getProvider().closeStatement(preparedStatement);
+            this.connection.getConnectionContext().getProvider().closeConnection(connection);
+        }
+    }
+
+    public void insertBatch(String query, IConnectionStatementConsumer statementConsumer, IConnectionResultConsumer resultConsumer) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet insertResult = null;
+
+        try {
+            connection = this.connection.get();
+            preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            statementConsumer.accept(preparedStatement);
+
+            preparedStatement.executeBatch();
+
+            insertResult = preparedStatement.getGeneratedKeys();
+
+            final IConnectionResult connectionResult = new ConnectionResult(insertResult);
+
+            while (insertResult.next()) {
+                resultConsumer.accept(connectionResult);
+            }
+        } catch (Exception e) {
+            this.logger.error(STR."Error while executing INSERT query: \{query}", e);
+        } finally {
+            this.connection.getConnectionContext().getProvider().closeResultSet(insertResult);
+            this.connection.getConnectionContext().getProvider().closeStatement(preparedStatement);
+            this.connection.getConnectionContext().getProvider().closeConnection(connection);
+        }
+    }
+
+    public void insert(String query, IConnectionResultConsumer consumer, Object... parameters) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet insertResult = null;
+
+        try {
+            connection = this.connection.get();
+            preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            this.setPreparedStatementParameters(preparedStatement, parameters);
+
+            preparedStatement.executeUpdate();
+
+            insertResult = preparedStatement.getGeneratedKeys();
+
+            final IConnectionResult connectionResult = new ConnectionResult(insertResult);
+
+            while (insertResult.next()) {
+                consumer.accept(connectionResult);
+            }
+        } catch (Exception e) {
+            this.logger.error(STR."Error while executing INSERT query: \{query}", e);
+        } finally {
+            this.connection.getConnectionContext().getProvider().closeResultSet(insertResult);
+            this.connection.getConnectionContext().getProvider().closeStatement(preparedStatement);
+            this.connection.getConnectionContext().getProvider().closeConnection(connection);
+        }
+    }
+
+    public void delete(String query, IConnectionResultConsumer consumer, Object... parameters) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        boolean isDeleted;
+
+        try {
+            connection = this.connection.get();
+            preparedStatement = connection.prepareStatement(query);
+
+            this.setPreparedStatementParameters(preparedStatement, parameters);
+
+            isDeleted = preparedStatement.executeUpdate() > 0;
+
+            consumer.accept(isDeleted);
+        } catch (Exception e) {
+            this.logger.error(STR."Error while executing DELETE query: \{query}", e);
+        } finally {
             this.connection.getConnectionContext().getProvider().closeStatement(preparedStatement);
             this.connection.getConnectionContext().getProvider().closeConnection(connection);
         }
