3398fa90194344376faad35e20b2a657d1353343
nicollassilva <lyod.hp@gmail.com>
2024-04-07 16:46:26 -0300

    Testing push 2

--- emulator/src/main/java/habbo/rooms/RoomManager.java
+++ emulator/src/main/java/habbo/rooms/RoomManager.java
@@ -12,7 +12,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 import storage.repositories.rooms.IRoomRepository;
-import storage.results.IConnectionResult;
 
 import java.util.ArrayList;
 import java.util.Collections;
